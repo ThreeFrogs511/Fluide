a92c98359e443ed7a444bb9a2c1ecea4591b01f9
ThreeFrogs511 <nicolas.lavarde@gmail.com>
2026-04-11 22:08:13 +0200

    fixing cache bug

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import FilterableArticleList from '@/components/FilterableArticleList'
 import { getArticles } from '@/lib/articles'
 
+export const revalidate = 0
+
 export default async function HomePage() {
   const articles = await getArticles()
 
+  console.log(articles)
   return (
     <>
       <header className="mb-12">
